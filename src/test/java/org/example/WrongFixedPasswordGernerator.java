9026611a54f29c9b91306cbf5322d2c172c95f4a
min-zi <min-zi@naver.com>
2023-08-02 22:41:58 +0900

    사칙 연산 계산기

--- src/test/java/org/example/WrongFixedPasswordGernerator.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class CorrectFixedPasswordGernerator implements PasswordGenerator {
-    @Override
-    public String generatePassword() {
-        return "abcdefgh"; // 8글자
-    }
-}
